a2e1b9f6f343bd53b66008d5313ae97b3f47b58e
Evandro Zeferino <egzefer@gmail.com>
2018-06-03 16:01:21 -0300

    Add configuration files

---
+++ src/main/java/com/egzefer/diffr/DiffrApplication.java
@@ -0,0 +1,12 @@
+package com.egzefer.diffr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiffrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiffrApplication.class, args);
+	}
+}
